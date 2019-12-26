17baf4c4d8710621ab0727748055bda625bc280d
SJH <jun9395@naver.com>
2019-12-26 20:59:10 +0900

    20191226

---
+++ src/CanIprint.java
@@ -0,0 +1,19 @@
+/***
+ * author info 
+ * name  : junhee son
+ * email : jun9395@naver.com
+ * phone : jjj
+ * description : 
+ *
+ *
+ **/
+public class CanIprint {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.print("반갑습니다");
+        System.out.print("이건 역슬래시 n이 없습니다\n");
+        System.out.println();
+        System.out.println("한 줄 내려버리기");
+        System.out.println(129);
+    }
+}
---
+++ src/HelloInteliJ.java
@@ -0,0 +1,14 @@
+/***
+ * author info 
+ * name  :
+ * email :
+ * phone :
+ * description : 
+ *
+ *
+ ***/
+public class HelloInteliJ {
+    public static void main(String[] args) { // 이것이 주석이다 실전편
+        System.out.println("Hello InteliJ");
+    }
+}
