0b7c786757dca60528e3cb375d3bff3db862cb63
jun9395 <jun9395@naver.com>
2020-12-19 00:11:44 +0900

    MovingAnt debugged

---
+++ first_java/hardQuest/src/MovingAnt.java
@@ -0,0 +1,56 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+
+public class MovingAnt {
+    public static void main(String[] args) {
+        String[][] table = new String[10][];
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int x = 1, y = 1;
+            for (int i = 0; i < 10; i++)
+                table[i] = br.readLine().split(" ");
+            if (table[x][y].equals("2"))
+                table[x][y] = "9";
+            else {
+                table[x][y] = "9";
+                boolean flag;
+                while (true) {
+                    flag = false;
+                    while (table[x][y + 1].equals("0")) {
+                        y++;
+                        table[x][y] = "9";
+                    }
+                    if (table[x][y + 1].equals("2")) {
+                        table[x][y + 1] = "9";
+                        break;
+                    }
+                    while (table[x + 1][y].equals("0")) {
+                        x++;
+                        table[x][y] = "9";
+                        if (table[x][y + 1].equals("0")) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (flag) continue;
+                    if (table[x + 1][y].equals("2")) {
+                        table[x + 1][y] = "9";
+                        break;
+                    }
+                    if (table[x][y + 1].equals("1"))
+                        break;
+                }
+            }
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++)
+                    bw.write(table[i][j] + " ");
+                bw.newLine();
+                bw.flush();
+            }
+            bw.close();
+        } catch (Exception e) {}
+    }
+}
